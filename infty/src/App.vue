36691f0b2a371f160bd4834de4f930064b5ddaca
Chungeng Meng <chungeng.meng@mail.utoronto.ca>
2021-07-14 00:11:09 -0400

    v3-

--- infty/src/App.vue
+++ infty/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <Navbar />
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Navbar from './components/Navbar.vue'
+
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Navbar
+    HelloWorld
   }
 }
 </script>
