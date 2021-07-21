458552825793997da29c00efe68ecdd84d74f7e4
Lizhi Yu <leedsyu@yahoo.com>
2021-07-20 23:19:00 -0400

    added models

---
+++ backend/src/models/nft.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const nftSchema = new Schema({
+    title: { type: String, required: true },
+    nft_id: { type: String, required: true },
+    description: { type: String },
+    file: { data: Buffer, contentType: String, required: true },
+
+    price: { type: Number, required: true },
+    currency: { type: String, required: true },
+    percentage: { type: Number, required: true },
+    
+    album_id: { type: String },
+    owner: { type: String },
+});
+
+const Nft = mongoose.model("Nft", nftSchema, "nfts");
+
+module.exports = Nft;
