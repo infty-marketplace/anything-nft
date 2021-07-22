3648179d580d079aaf97049fbd0ec5a8900d13e7
Jacob <jiahuanglin27@gmail.com>
2021-07-22 11:27:02 -0400

    added user profile api implementation

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,8 +1,20 @@
-authUser = async (req, res) => {
+const { User } = require('../models')
+const ApiError = require('../utils/ApiError');
+
+const authUser = async (req, res) => {
     return res.status(200).send();
 };
 
+const getUser = async (req, res) => {
+    const user = await User.findOne(req.params.address);
+    if (!user) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    }
+    res.send(user);
+}
+
 
 module.exports = {
-    authUser
+    authUser,
+    getUser
 };
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,15 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    address: { type: String, required: true },
+    first_name: { type: String, required: true, trim: true },
+    last_name: { type: String, required: true, trim: true },
+    address: { type: String, required: true, trim: true },
     description: { type: String },
     nft_ids: { type: [String] },
     album_ids: { type: [String] },
     profile_picture: { data: Buffer, contentType: String },
-
 });
+
+/**
+ * Check if address is taken
+ * @param {string} address - The user's address
+ * @returns {Promise<boolean>}
+ */
+userSchema.statics.isAddressTaken = async function (address) {
+    const user = await this.findOne({ address });
+    return !!user;
+};
 
 const User = mongoose.model("User", userSchema, "users");
 
