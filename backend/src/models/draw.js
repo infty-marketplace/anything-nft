d8b9beca987b556f2053a7727b14757c74caef8a
Lizhi Yu <leedsyu@yahoo.com>
2021-07-30 00:21:49 -0400

    updated models

--- backend/src/models/draw.js
+++ backend/src/models/draw.js
@@ -4,15 +4,23 @@ const Schema = mongoose.Schema;
 const drawSchema = new Schema({
     title: { type: String, required: true },
     draw_id: { type: String, required: true },
-    description: { type: String },
+    description: { type: String, default: '' },
 
     unit_price: { type: Number, required: true },
     quantity: { type: Number },
     currency: { type: String, required: true },
     deadline: { type: Number },
-    
-    nft_id: { type: String },
-    owner: { type: String },
+
+    nft_id: { type: String, require: true },
+    owner: { type: String, require: true },
+    participant: { type: [{ address: String, unit: Number }], default: [] },
+
+}, {
+    timestamps: {
+        currentTime: () => Math.floor(Date.now() / 1000),
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
 });
 
 const Draw = mongoose.model("Draw", drawSchema, "draws");
