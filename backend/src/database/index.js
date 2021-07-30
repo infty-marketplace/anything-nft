d925d907c2027c093ff202adefc51c2a9bb372f7
Lizhi Yu <39842567+yuliwilliam@users.noreply.github.com>
2021-07-30 00:23:51 -0400

    Updated backend db models (#29)
    * init backend structure
    
    * installed dev dep
    
    * added models
    
    * added models
    
    * edited schemas
    
    * update schema
    
    * updated db role
    
    * updated db role
    
    * updated models

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -8,5 +8,6 @@ mongoose
     .catch(e => {
         console.error("Connection error", e.message);
     });
+const db = mongoose.connection;
 
 module.exports = db;
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -3,13 +3,19 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
+    last_name: { type: String },
     address: { type: String, required: true },
-    description: { type: String },
-    nft_ids: { type: [String] },
-    album_ids: { type: [String] },
+    description: { type: String, default: '' },
+    nft_ids: { type: [String], default: [] },
+    album_ids: { type: [String], default: [] },
     profile_picture: { data: Buffer, contentType: String },
 
+}, {
+    timestamps: {
+        currentTime: () => Math.floor(Date.now() / 1000),
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
 });
 
 const User = mongoose.model("User", userSchema, "users");
