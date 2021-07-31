b23df15aa0cad6e0879c89423ce1832aa1c3ba39
Lizhi Yu <39842567+yuliwilliam@users.noreply.github.com>
2021-07-31 00:44:45 -0400

    Defined models' fields name (#33)
    * init backend structure
    
    * installed dev dep
    
    * added models
    
    * added models
    
    * edited schemas
    
    * update schema
    
    * updated db role
    
    * updated db role
    
    * updated models
    
    * fixed comma
    
    * added constant file
    
    * fixed typo updated doc
    
    * updated doc

--- backend/src/models/draw.js
+++ backend/src/models/draw.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const drawSchema = new Schema({
-    title: { type: String, required: true },
-    draw_id: { type: String, required: true },
-    description: { type: String, default: '' },
+const drawSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        draw_id: { type: String, required: true },
+        description: { type: String, default: "" },
 
-    unit_price: { type: Number, required: true },
-    quantity: { type: Number },
-    currency: { type: String, required: true },
-    deadline: { type: Number },
+        unit_price: { type: Number, required: true },
+        quantity: { type: Number },
+        currency: { type: String, required: true },
+        deadline: { type: Number },
 
-    nft_id: { type: String, require: true },
-    owner: { type: String, require: true },
-    participant: { type: [{ address: String, unit: Number }], default: [] },
-
-}, {
-    timestamps: {
-        currentTime: () => Math.floor(Date.now() / 1000),
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
+        nft_id: { type: String, require: true },
+        owner: { type: String, require: true },
+        participants: {
+            type: [{ address: String, quantity: Number }],
+            default: [],
+        },
+    },
+    {
+        timestamps: {
+            currentTime: () => Math.floor(Date.now() / 1000),
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+        },
     }
-});
+);
 
 const Draw = mongoose.model("Draw", drawSchema, "draws");
 
