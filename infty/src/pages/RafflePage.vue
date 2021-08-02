af53c976b4837e8f1b9e1d35335ebf1a79ef3574
Violet-coder <violetzzy6@gmail.com>
2021-08-02 16:44:42 -0400

    raffle page banner

--- infty/src/pages/RafflePage.vue
+++ infty/src/pages/RafflePage.vue
@@ -1,14 +1,172 @@
 <template>
   <div class="raffle-page">
     <Navbar active-index="1" />
-    <div class="container">
-      <div id="stack">
-        <div id="card">
-          <figure class="front spade"></figure>
-          <figure class="back"></figure>
+    <div>
+      <div id="sidebar" style="width: 25%">
+        <b-card no-body style="max-width: 20rem">
+          <template #header>
+            <h4 class="mb-0">
+              <b-icon icon="filter-circle"></b-icon>&nbsp;Filter
+            </h4>
+          </template>
+          <b-list-group flush>
+            <b-list-group-item>
+              <b-button
+                pill
+                v-b-toggle.collapse-1
+                variant="outline-secondary"
+                class="category-button"
+                >Status</b-button
+              >
+              <b-collapse id="collapse-1" class="mt-2">
+                <b-card>
+                  <b-form-group v-slot="{ ariaDescribedby }">
+                    <b-form-checkbox-group
+                      id="checkbox-group-1"
+                      v-model="statusSelected"
+                      :options="statusOptions"
+                      :aria-describedby="ariaDescribedby"
+                      name="flavour-1"
+                    ></b-form-checkbox-group>
+                  </b-form-group>
+                </b-card>
+              </b-collapse>
+            </b-list-group-item>
+            <b-list-group-item>
+              <b-button
+                pill
+                v-b-toggle.collapse-2
+                variant="outline-secondary"
+                class="category-button"
+                >Price</b-button
+              >
+              <b-collapse id="collapse-2" class="mt-2">
+                <b-card>
+                  <b-form-select
+                    v-model="priceTypeSelected"
+                    :options="priceTypeOptions"
+                  ></b-form-select>
+                  <b-form-input
+                    v-model="text"
+                    placeholder="Min"
+                    class="price-range"
+                  ></b-form-input>
+                  <span>to</span>
+                  <b-form-input
+                    v-model="text"
+                    placeholder="Max"
+                    class="price-range"
+                  ></b-form-input>
+                  <b-button id="price-apply-btn" variant="outline-primary"
+                    >Apply</b-button
+                  >
+                </b-card>
+              </b-collapse>
+            </b-list-group-item>
+            <b-list-group-item>
+              <b-button
+                pill
+                v-b-toggle.collapse-3
+                variant="outline-secondary"
+                class="category-button"
+                >Collections</b-button
+              >
+              <b-collapse id="collapse-3" class="mt-2">
+                <b-card>
+                  <b-input-group size="sm" class="mb-2">
+                    <b-input-group-prepend is-text>
+                      <b-icon icon="search"></b-icon> </b-input-group-prepend
+                    ><b-form-input placeholder="Filter"></b-form-input>
+                  </b-input-group>
+
+                  <b-list-group id="collections-group">
+                    <b-list-group-item>Bored Ape Yacht Club</b-list-group-item>
+                    <b-list-group-item>CrptoPunks</b-list-group-item>
+                    <b-list-group-item>Art Blocks Curated</b-list-group-item>
+                    <b-list-group-item>Bored Ape Kennel Club</b-list-group-item>
+                    <b-list-group-item>SupDucks</b-list-group-item>
+                    <b-list-group-item>Cool Cat</b-list-group-item>
+                    <b-list-group-item>ZED RUN</b-list-group-item>
+                    <b-list-group-item>Famelady</b-list-group-item>
+                  </b-list-group>
+                </b-card>
+              </b-collapse>
+            </b-list-group-item>
+            <b-list-group-item>
+              <b-button
+                pill
+                v-b-toggle.collapse-4
+                variant="outline-secondary"
+                class="category-button"
+                >Chains</b-button
+              >
+              <b-collapse id="collapse-4" class="mt-2">
+                <b-card>
+                  <b-list-group>
+                    <b-list-group-item>Ethereum</b-list-group-item>
+                    <b-list-group-item>Polygon</b-list-group-item>
+                    <b-list-group-item>Klaytn</b-list-group-item>
+                  </b-list-group>
+                </b-card>
+              </b-collapse>
+            </b-list-group-item>
+            <b-list-group-item>
+              <b-button
+                pill
+                v-b-toggle.collapse-5
+                variant="outline-secondary"
+                class="category-button"
+                >Categories</b-button
+              >
+              <b-collapse id="collapse-5" class="mt-2">
+                <b-card>
+                  <b-list-group>
+                    <b-list-group-item>Art</b-list-group-item>
+                    <b-list-group-item>Music</b-list-group-item>
+                    <b-list-group-item>Domain Names</b-list-group-item>
+                    <b-list-group-item>Virtual Worlds</b-list-group-item>
+                    <b-list-group-item>Trading Cards</b-list-group-item>
+                    <b-list-group-item>Collectibles</b-list-group-item>
+                  </b-list-group>
+                </b-card>
+              </b-collapse>
+            </b-list-group-item>
+          </b-list-group>
+        </b-card>
+      </div>
+      <div class="pool">
+        <div id="banner">
+          <img
+            src="@/assets/imgs/currency.png"
+            alt="Girl in a jacket"
+            width="100"
+            height="100"
+          />
+          <!-- <div>
+            Icons made by
+            <a href="https://www.freepik.com" title="Freepik">Freepik</a> from
+            <a href="https://www.flaticon.com/" title="Flaticon"
+              >www.flaticon.com</a
+            >
+          </div> -->
+          <h2 id="banner-content">Currently 175 people participated!</h2>
+        </div>
+        <div class="mt-4">
+          <b-card
+            img-src="https://placekitten.com/300/300"
+            img-alt="Card image"
+            img-left
+            class="mb-3"
+          >
+            <b-card-text>
+              Some quick example text to build on the card and make up the bulk
+              of the card's content.
+            </b-card-text>
+          </b-card>
         </div>
       </div>
     </div>
+
     <Footer />
   </div>
 </template>
@@ -16,6 +174,7 @@
 <script>
 import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
+
 export default {
   name: "RafflePage",
   components: {
@@ -25,108 +184,40 @@ export default {
 };
 </script>
 <style  scoped>
-@charset "UTF-8";
+@import url("https://fonts.googleapis.com/css2?family=Palette+Mosaic&display=swap");
 .raffle-page {
-  text-align: left;
+  background-color: black;
 }
-.container {
-  width: 500px;
-  height: 500px;
-  /* position: relative; */
-  perspective: 800px;
+#sidebar {
   margin-top: 2em;
-  margin-left: 2em;
+  display: inline-block;
+  /* height: 100%; */
+  text-align: left;
+  vertical-align: top;
 }
+#banner {
+  margin-top: 2em;
+  border-radius: 10px;
+  background-image: linear-gradient(-20deg, #6e45e2 0%, #88d3ce 100%);
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  flex-basis: 20px;
+}
+#banner-content {
+  margin-left: 5px;
+  /* width: 70%; */
+  color: white;
+  font-family: "Palette Mosaic", cursive;
+}
+.pool {
+  width: 60%;
+  margin-left: 5%;
 
-#stack {
-  background: url(https://www.toptal.com/designers/subtlepatterns/patterns/pink%20dust.png);
-  height: 325px;
-  width: 250px;
-  border-radius: 10px;
-  position: absolute;
-  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
-  box-sizing: border-box;
-  padding: 10px;
-  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
-  transform: rotate(2deg);
-}
-#stack:before {
-  top: -2px;
-  left: -2px;
-  transform: rotate(-1deg);
-}
-#stack:after {
-  top: -5px;
-  left: -6px;
-  transform: rotate(-1.5deg);
-}
-#stack:before,
-#stack:after {
-  content: "";
-  background: url(https://www.toptal.com/designers/subtlepatterns/patterns/pink%20dust.png);
-  height: 325px;
-  width: 250px;
-  display: block;
-  border-radius: 10px;
-  position: absolute;
-  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
-}
-#stack #card {
-  width: 250px;
-  height: 325px;
-  position: absolute;
-  transform-style: preserve-3d;
-  transition: transform 0.4s ease, margin 0.6s ease 0.2s;
-  left: -3px;
-  top: -3px;
-}
-#stack #card figure {
-  display: block;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-  border-radius: 10px;
-  transition-delay: transform 1s;
-}
-#stack #card figure.front {
-  background: #fafafa;
-  transform: rotateZ(2deg) rotateY(180deg);
-}
-#stack #card figure.front:after {
-  color: #555;
-  font-size: 120px;
-  font-family: arial;
-  position: absolute;
-  top: 80px;
-}
-#stack #card figure.front.spade:after {
-  content: "♠";
-  left: 95px;
-}
-#stack #card figure.front.club:after {
-  content: "♣";
-  left: 85px;
-}
-#stack #card figure.front.heart:after {
-  content: "❤";
-  left: 70px;
-  color: rgba(255, 90, 25, 0.8);
-}
-#stack #card figure.front.diamond:after {
-  content: "♦";
-  left: 95px;
-  color: rgba(255, 90, 25, 0.8);
-}
-#stack #card figure.back {
-  background: url(https://www.toptal.com/designers/subtlepatterns/patterns/pink%20dust.png);
-}
-#stack:hover #card {
-  margin: -8px 0px 0px 300px;
-  transform: rotateY(-180deg);
-  transition: transform 0.6s ease 0.4s, margin 0.8s ease;
-}
-#stack:hover #card figure {
-  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
+  display: inline-block;
+
+  vertical-align: top;
 }
 </style>
