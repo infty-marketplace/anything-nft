90c59e547e9188861ea627bb217eb8508d570df7
Lizhi Yu <leedsyu@yahoo.com>
2021-08-05 18:01:13 -0400

    refactor

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,7 +2,6 @@ authUser = async (req, res) => {
     return res.status(200).send();
 };
 
-
 module.exports = {
-    authUser
+    authUser,
 };
--- backend/src/routes/collectionRouter.js
+++ backend/src/routes/collectionRouter.js
@@ -1,156 +1,25 @@
 const express = require("express");
 const collectionController = require("../controllers/collectionController");
-const Nft = require("../models/nft");
-const Album = require("../models/album");
-const Draw = require("../models/draw");
-const constants = require("../constants/index");
-const mongoose = require("mongoose");
-const multipart = require('connect-multiparty');
-const fs = require("fs");
-
-const { makeid } = require('../utils/helpers')
-
-require('dotenv').config();
+const multipart = require("connect-multiparty");
 
 const multipartMiddleware = multipart();
-
-mongoose.set('useFindAndModify', false);
-
 const router = express.Router();
-const AWS = require('aws-sdk');
-AWS.config.update({
-    region: 'us-east-1',
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
-});
 
-const s3 = new AWS.S3();
+router.get("/nft/:nft_id", () => {});
+router.get("/album/:album_id", () => {});
+router.get("/draw/:draw_id", () => {});
+router.post("/market", () => {});
 
-router.get("/nft/:nft_id", ()=>{});
-router.get("/album/:album_id", ()=>{});
-router.get("/draw/:draw_id", ()=>{});
-router.post("/market", ()=>{});
+router.post("/create-nft", multipartMiddleware, collectionController.createNft);
+router.post("/list-nft", collectionController.listNft);
+router.post("/list-nft-draw", collectionController.listNftDraw);
+router.post("/create-album", collectionController.createAlbum);
+router.post("/list-album", collectionController.listAlbum);
 
-router.post("/create-nft", multipartMiddleware, (req, res, next)=>{
-    console.log("Create NFT")
-    const nftId = makeid(5);
-    const params = {
-        title: req.body.title,
-        nft_id: nftId,
-        description: req.body.description,
-        file: null,
+router.post("/purchase-nft", () => {});
+router.post("/draw-nft", () => {});
+router.post("/fund-nft", () => {});
 
-        status: constants.STATUS_PRIVATE,
-
-        owner: [{'address': req.body.address, 'percentage': 100}],
-    };
-
-    const tmp_path = req.files.file.path;
-    const fileToUpload = fs.createReadStream(tmp_path);
-    const s3UploadParams = {Bucket: process.env.S3_BUCKET_NAME, Key: nftId, Body: fileToUpload}; 
-
-    s3.upload(s3UploadParams, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        const uploadedUrl = data.Location;
-        params.file = uploadedUrl;
-        const newNFT = new Nft(params);
-        newNFT.save(function (err){
-            if (err) {
-                return res.status(400).send(err);
-            }
-            return res.send("File uploaded successfully");
-        });
-    });
-});
-
-router.post("/list-nft", (req, res, next)=>{
-    const nftId = req.body.nftId;
-    Nft.findOneAndUpdate({"nft_id" : nftId},
-        {
-            status: constants.STATUS_SALE,
-            price: req.body.price,
-            currency: req.body.currency
-        }, (err) => {
-        if (err) {
-            return res.status(400).send(err);
-        }
-        return res.send("Status changed to sale");
-    });
-});
-
-router.post("/list-nft-draw", (req, res, next)=>{
-    const nftId = req.body.nftId;
-
-    // create draw document
-    const drawParams = {
-        title: req.body.title,
-        draw_id: makeid(5),
-        description: req.body.description,
-
-        unit_price: req.body.unitPrice,
-        quantity: req.body.quantity,
-        currency: req.body.currency,
-        deadline: req.body.deadline,
-
-        nft_id: req.body.nftId,
-        owner: req.body.owner,
-        participants: [],
-    };
-
-    const newDraw = new Draw(drawParams);
-    newDraw.save(function (err){
-        if (err) {
-            return res.status(400).send(err);
-        }
-        Nft.findOneAndUpdate({"nft_id" : nftId}, {'status': constants.STATUS_DRAW}, function(err1){
-            if (err1){
-                return res.status(500).send(err1);
-            }
-            return res.send("Draw document created and status changed to draw");
-        });
-    });
-});
-
-router.post("/create-album", (req, res, next)=>{
-    const params = {
-        title: req.body.title,
-        album_id: req.body.albumId,
-        description: req.body.description,
-        file: req.body.file,
-        status: req.body.status,
-
-        price: req.body.price,
-        currency: req.body.currency,
-        
-        nft_ids: req.body.nftIds,
-        owner: req.body.userId
-    };
-    const newAlbum = new Album(params);
-    newAlbum.save(function (err){
-        if (err) {
-            return res.send(err);
-        }
-        return res.send("Album created");
-    });
-});
-
-router.post("/list-album", (req, res, next)=>{
-    const albumId = req.body.albumId;
-    Album.findOneAndUpdate({"album_id": albumId}, {'status': constants.STATUS_SALE}, function(err){
-        if (err){
-            return res.send(err);
-        }
-        return res.send("Album status changed to sale");
-    });
-});
-
-
-router.post("/purchase-nft", ()=>{});
-router.post("/draw-nft", ()=>{});
-router.post("/fund-nft", ()=>{});
-
-router.post("/purchase-album", ()=>{});
+router.post("/purchase-album", () => {});
 
 module.exports = router;
