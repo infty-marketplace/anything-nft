4c2da171eb09547910e2e05b755598c9d7dfee4b
Chungeng Meng <chungeng.meng@mail.utoronto.ca>
2021-08-07 18:01:14 -0400

    save

--- infty/src/pages/Collections.vue
+++ infty/src/pages/Collections.vue
@@ -1,15 +1,19 @@
 <template>
     <div class='flex-wrapper'>
         <Navbar active-index='2'/>
-        <div class='flex-wrapper-row m-3'>
+        <div class='flex-wrapper-row m-3' v-if='$store.getters.getAddress'>
         <b-tabs class='main-content' content-class="ml-5 mr-5">
             <b-tab title="NFT" active>
                 <div class='cards-container'>
                 <Card class='mt-4 card' v-for="nft in nfts" :card="nft" :key="nft.url" />
+                <p class='mt-4' v-if="nfts.length == 0">Nothing</p>
                 </div>
             </b-tab>
             <b-tab title="Album"><p>TODO</p></b-tab>
         </b-tabs>
+        </div>
+        <div v-else class='flex-wrapper-row'>
+          <ConnectWallet />
         </div>
         <Footer style="z-index:0"/>
         <a href='/mine/create'>
@@ -22,118 +26,50 @@
 
 <script>
 import axios from 'axios'
+import { eventBus } from '../main'
 
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
 import Card from '../components/Card.vue'
+import ConnectWallet from '../components/ConnectWallet.vue'
+
 export default {
     name: "Collections",
     components: {
         Navbar,
         Footer,
-        Card
+        Card,
+        ConnectWallet
     },
     data: () => ({
-        nfts: [
-        {
-          collection: "SupDucks",
-          author: "Ada Lovelace",
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-        },
-        {
-          collection: "Art Blocks Curated",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        {
-          collection: "Bored Ape Kennel Club",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        {
-          collection: "Cool Cats",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        {
-          collection: "ZED RUN",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        {
-          collection: "FameLadySquad",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        {
-          collection: "Sorare",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        {
-          collection: "Meebits",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        {
-          collection: "Ape Gang",
-          author: "Ada Lovelace",
-          price: Math.random().toFixed(2),
-          url: `https://source.unsplash.com/random/200x200?sig=1${Math.round(
-            Math.random() * 100
-          )}`,
-          expirationDate: `${Math.round(Math.random() * 10)}`,
-        },
-        ]
+        nfts: []
     }),
     async mounted() {
-      const getters = this.$store.getters
-      const accounts = await window.conflux.send("cfx_requestAccounts")
-      this.$store.commit("setAccount", accounts[0])
-      const res = await axios.get(`${getters.getApiUrl}/profile/${getters.getAccount}`)
-      const nft_promises = res.data.nft_ids.map((nid) => axios.get(`${getters.getApiUrl}/nft/${nid}`))
-      const nft_promises_result = await Promise.allSettled(nft_promises)
-      let nfts = nft_promises_result.map(p => {
-        if (p.status == 'fulfilled') {
-          return p.value.data;
-        }
-      })
-      nfts = nfts.map(n => {
-        n.url = n.file;
-        n.author = n.owner[0].address
-        return n
-      })
-      this.nfts = nfts;
+      if (this.$store.getters.getAddress) this.loadNfts();
+      eventBus.$on("Collections.loadNfts", ()=>this.loadNfts())
+      this.$store.dispatch('connectWallet')
+    },
+    beforeDestroy() {
+      eventBus.$off("Collections.loadNfts")
+    },
+    methods: {
+      async loadNfts() {
+        const getters = this.$store.getters
+        const res = await axios.get(`${getters.getApiUrl}/profile/${getters.getAddress}`)
+        const nft_promises = res.data.nft_ids.map((nid) => axios.get(`${getters.getApiUrl}/nft/${nid}`))
+        const nft_promises_result = await Promise.allSettled(nft_promises)
+        let nfts = nft_promises_result.map(p => {
+          if (p.status == 'fulfilled') {
+            return p.value.data;
+          }
+        })
+        nfts = nfts.map(n => {
+          n.url = n.file;
+          n.author = n.owner[0].address
+          return n
+        })
+        this.nfts = nfts;
+      }
     }
 }
 </script>
