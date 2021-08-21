0432ffed57bbb030c087748b78357ce17eeb32c7
Lizhi Yu <leedsyu@yahoo.com>
2021-08-20 23:51:00 -0400

    update

--- conflux/raffle/contracts/Raffle.sol
+++ conflux/raffle/contracts/Raffle.sol
@@ -15,31 +15,29 @@ contract Raffle {
     uint price;
     address[] participants;
     address owner;
-    uint256 tokenId;
     }
   
   address internal nftContract = address(keccak256("cfxtest:ace5gcmv1x118ts2tta4k83asp7sxrz566w4defuhr"));
 
   address public manager;
-  mapping(address => RaffleInfo) public raffles;
+  mapping(uint256 => RaffleInfo) public raffles;
 
-  event CreateEvent(address _nft, address _owner, uint _maxTickets, uint _price);
-  event JoinEvent(address _nft, address _participant, uint _qty);
-  event DrawEvent(address _nft, address _winner, uint _nftValue);
-  event AbortEvent(address _nft, address _owner);
+  event CreateEvent(uint256 _tokenId, address _owner, uint _maxTickets, uint _price);
+  event JoinEvent(uint256 _tokenId, address _participant, uint _qty);
+  event DrawEvent(uint256 _tokenId, address _winner, uint _nftValue);
+  event AbortEvent(uint256 _tokenId, address _owner);
   event Paid(address _from, uint _value);
 
   function Raffle() public payable {
     manager = msg.sender;
   }
 
-  function createRaffle(uint _maxTickets, uint _price, address _owner, address _nft, uint256 _tokenId) public payable {
-    raffles[_nft].maxTickets = _maxTickets;
-    raffles[_nft].price = _price;
-    raffles[_nft].owner = _owner;
-    raffles[_nft].tokenId = _tokenId;
+  function createRaffle(uint _maxTickets, uint _price, address _owner, uint256 _tokenId) public payable {
+    raffles[_tokenId].maxTickets = _maxTickets;
+    raffles[_tokenId].price = _price;
+    raffles[_tokenId].owner = _owner;
 
-    emit CreateEvent(_nft, _owner, _maxTickets, _price);
+    emit CreateEvent(_tokenId, _owner, _maxTickets, _price);
   }
 
   // `fallback` function called when eth is sent to Payable contract
@@ -48,45 +46,43 @@ contract Raffle {
   }
 
   // purchase tickets
-  function joinRaffle(address _nft, uint _qty) public payable returns(bool) {
-    require(msg.value == raffles[_nft].price * _qty, "message value is not equal to tickets' total price");
-    require(int(raffles[_nft].participants.length) <= int(raffles[_nft].maxTickets - _qty), "raffle does not have enough tickets left");
+  function joinRaffle(uint256 _tokenId, uint _qty) public payable returns(bool) {
+    require(msg.value == raffles[_tokenId].price * _qty, "message value is not equal to tickets' total price");
+    require(int(raffles[_tokenId].participants.length) <= int(raffles[_tokenId].maxTickets - _qty), "raffle does not have enough tickets left");
 
     for (uint i = 0; i < _qty; i++) {
-      raffles[_nft].participants.push(msg.sender);
+      raffles[_tokenId].participants.push(msg.sender);
     }
 
-    emit JoinEvent (_nft, msg.sender, _qty);
+    emit JoinEvent (_tokenId, msg.sender, _qty);
     
-    if (raffles[_nft].participants.length == raffles[_nft].maxTickets) {
-      return draw(_nft);
+    if (raffles[_tokenId].participants.length == raffles[_tokenId].maxTickets) {
+      return draw(_tokenId);
     }
     return true;
   }
 
   // award nftValue when all tickets are sold
-  function draw(address _nft) public restricted returns (bool) {
+  function draw(uint256 _tokenId) public restricted returns (bool) {
 
-    if (raffles[_nft].participants.length > 0){
+    if (raffles[_tokenId].participants.length > 0){
       uint seed = block.number;
-      uint random = uint(keccak256(seed)) % raffles[_nft].participants.length;
-      address winner = raffles[_nft].participants[random];
-      uint nftValue = raffles[_nft].participants.length * raffles[_nft].price;
+      uint random = uint(keccak256(seed)) % raffles[_tokenId].participants.length;
+      address winner = raffles[_tokenId].participants[random];
+      uint nftValue = raffles[_tokenId].participants.length * raffles[_tokenId].price;
       uint commission = nftValue / 10 * 9;
 
       address(manager).transfer(commission);
-      address(raffles[_nft].owner).transfer(nftValue - commission);
+      address(raffles[_tokenId].owner).transfer(nftValue - commission);
       // transfer nft to winner
-      // function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes data) external payable;
-      // nftContract.call(bytes4(sha3("safeTransferFrom(address, address, uint256)")), (address(this), winner, raffles[_nft].tokenId));
-      transferNft(winner, raffles[_nft].tokenId);
-      delete raffles[_nft];
-      emit DrawEvent (_nft ,winner, nftValue);
+      transferNft(winner, _tokenId);
+      delete raffles[_tokenId];
+      emit DrawEvent (_tokenId ,winner, nftValue);
     }else{
       // transfer nft to owner
-      transferNft(raffles[_nft].owner, raffles[_nft].tokenId);
-      delete raffles[_nft];
-      emit AbortEvent(_nft, raffles[_nft].owner);
+      transferNft(raffles[_tokenId].owner, _tokenId);
+      delete raffles[_tokenId];
+      emit AbortEvent(_tokenId, raffles[_tokenId].owner);
     }
 
     return true;
@@ -98,8 +94,8 @@ contract Raffle {
 
   }
 
-  function getParticipants(address _nft) public view returns (address []) {
-    return raffles[_nft].participants;
+  function getParticipants(uint256 _tokenId) public view returns (address []) {
+    return raffles[_tokenId].participants;
   }
 
   modifier restricted() {
