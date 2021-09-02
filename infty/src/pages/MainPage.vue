58cf23bbf37dbc9a2cb1497151b3e11b52258bfe
Violet-coder <violetzzy6@gmail.com>
2021-09-02 01:00:05 -0400

    wave

--- infty/src/pages/MainPage.vue
+++ infty/src/pages/MainPage.vue
@@ -11,6 +11,20 @@
         >Create</b-button
       >
     </div>
+    <svg class="editorial" viewBox="0 24 150 28" preserveAspectRatio="none">
+      <defs>
+        <path
+          id="gentle-wave"
+          d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+        />
+      </defs>
+      <g class="parallax">
+        <use xlink:href="#gentle-wave" x="50" y="2" fill="#95d0f9" />
+        <use xlink:href="#gentle-wave" x="50" y="6" fill="#547186" />
+        <use xlink:href="#gentle-wave" x="50" y="12" fill="#121212" />
+      </g>
+    </svg>
+
     <Footer />
   </div>
 </template>
@@ -41,5 +55,37 @@ export default {
 .banner-text {
   font-size: 2rem;
   font-weight: 1000;
+}
+.editorial {
+  display: block;
+  width: 100%;
+  height: 10em;
+  max-height: 100vh;
+  margin: 0;
+}
+.parallax > use {
+  animation: move-forever 12s linear infinite;
+}
+.parallax > use:nth-child(1) {
+  /* animation: move-forever -2s; */
+  animation-delay: 2s;
+  /* animation-delay: -2s; */
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -2s;
+  animation-duration: 5s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 3s;
+}
+
+@keyframes move-forever {
+  0% {
+    transform: translate(-90px, 0%);
+  }
+  100% {
+    transform: translate(85px, 0%);
+  }
 }
 </style>
