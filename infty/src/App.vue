9d4c65abe7f427da8113116d2c31642af6b8cc7d
Lizhi Yu <39842567+yuliwilliam@users.noreply.github.com>
2021-09-02 00:27:44 -0400

    Staking page (#79)
    * update
    
    * staking page
    
    * done staking page
    
    * typo
    
    * fixed
    
    * save
    
    * fixed
    
    Co-authored-by: Chungeng Meng <chungeng.meng@mail.utoronto.ca>

--- infty/src/App.vue
+++ infty/src/App.vue
@@ -1,69 +1,79 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
 
 <script>
-const { abi } = require('./assets/InftyNft.json')
-import { eventBus } from './main'
+const { abi: inftyNftAbi } = require("./assets/InftyNft.json");
+const { abi: stakeAbi } = require("./assets/StakingForINFT.json");
+
+import { eventBus } from "./main";
 // const { Conflux } = require('js-conflux-sdk')
 
-
 export default {
-  name: "App",
-  created() {
-    // TODO based on wallet network
-    // const cfx = new Conflux({
-    //   url:'https://test.confluxrpc.com',
-    //   networkId: 1
-    // })
-    document.title = "Infty Marketplace";
-    const minterContract = window.confluxJS.Contract({abi, address:this.$store.getters.getMinterAddress})
-    this.$store.commit('setMinterContract', minterContract)
+    name: "App",
+    created() {
+        // TODO based on wallet network
+        // const cfx = new Conflux({
+        //   url:'https://test.confluxrpc.com',
+        //   networkId: 1
+        // })
+        document.title = "Infty Marketplace";
+        const minterContract = window.confluxJS.Contract({
+            abi: inftyNftAbi,
+            address: this.$store.getters.getMinterAddress,
+        });
+        this.$store.commit("setMinterContract", minterContract);
 
-    eventBus.$on("App.notifyWIP", () => {
-      this.$notify.info({
-        title: 'Info',
-        message: 'This functionality is work in progress.',
-        duration: 3000
-      })
-    })
-    eventBus.$on("App.notifyCommission", () => {
-      this.$notify({
-        title: 'Notification',
-        message: 'Paying commission now.',
-        duration: 0
-      })
-    })
-    
-  },
-  beforeDestroy() {
-    eventBus.$off("App.notifyWIP")
-    eventBus.$off("App.notifyCommission")
-  }
+        const stakeContract = window.confluxJS.Contract({
+            abi: stakeAbi,
+            address: this.$store.getters.getStakeAddress,
+        });
+        this.$store.commit("setStakeContract", stakeContract);
+
+        eventBus.$on("App.notifyWIP", () => {
+            this.$notify.info({
+                title: "Info",
+                message: "This functionality is work in progress.",
+                duration: 3000,
+            });
+        });
+
+        eventBus.$on("App.notifyCommission", () => {
+            this.$notify({
+                title: "Notification",
+                message: "Paying commission now.",
+                duration: 0,
+            });
+        });
+    },
+    beforeDestroy() {
+        eventBus.$off("App.notifyWIP");
+        eventBus.$off("App.notifyCommission");
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
 }
 
 .flex-wrapper {
-  display: flex;
-  min-height: 100vh;
-  flex-direction: column;
-  justify-content: space-between;
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+    justify-content: space-between;
 }
 
 .flex-wrapper-row {
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-around;
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-around;
 }
 </style>
