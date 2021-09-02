864c891fc577cd989e56260e3594ebcf7ddf0d38
Lizhi Yu <leedsyu@yahoo.com>
2021-09-02 00:21:04 -0400

    fixed

--- backend/src/utils/cfxUtils.js
+++ backend/src/utils/cfxUtils.js
@@ -9,11 +9,11 @@ const cfx = new Conflux({
     networkId: 1,
 });
 const managerAccount = cfx.wallet.addPrivateKey(process.env.MANAGER_KEY);
-const { minterAbi } = require("../assets/InftyNft.json");
-const { raffleAbi } = require("../assets/Raffle.json");
+const { abi: minterAbi } = require("../assets/InftyNft.json");
+const { abi: raffleAbi } = require("../assets/Raffle.json");
 
-const minterContract = cfx.Contract({ minterAbi, address: process.env.MINTER_ADDRESS });
-const raffleContract = cfx.Contract({ raffleAbi, address: process.env.MINTER_ADDRESS });
+const minterContract = cfx.Contract({ abi: minterAbi, address: process.env.MINTER_ADDRESS });
+const raffleContract = cfx.Contract({ abi: raffleAbi, address: process.env.MINTER_ADDRESS });
 
 async function nextTokenId() {
     return (await minterContract.totalSupply()) + 1n;
@@ -52,14 +52,17 @@ async function generateUri(req, imageUri, sha) {
     const metaData = {
         name: req.body.title,
         description: req.body.description,
-        image: imageUri
-    }
-    const data = await s3.putObject({
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: sha,
-        Body: JSON.stringify(metaData),
-        ContentType: "application/json"}).promise()
-    return `https://conflux-infty.s3.amazonaws.com/${sha}`
+        image: imageUri,
+    };
+    const data = await s3
+        .putObject({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: sha,
+            Body: JSON.stringify(metaData),
+            ContentType: "application/json",
+        })
+        .promise();
+    return `https://conflux-infty.s3.amazonaws.com/${sha}`;
 }
 
 async function generateAlbumUri(req, imageUri, sha) {
@@ -67,14 +70,17 @@ async function generateAlbumUri(req, imageUri, sha) {
         name: req.body.title,
         description: req.body.description,
         image: imageUri,
-        nft_ids: JSON.parse(req.body.nft_ids)
-    }
-    const data = await s3.putObject({
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: sha,
-        Body: JSON.stringify(metaData),
-        ContentType: "application/json"}).promise()
-    return `https://conflux-infty.s3.amazonaws.com/${sha}`
+        nft_ids: JSON.parse(req.body.nft_ids),
+    };
+    const data = await s3
+        .putObject({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: sha,
+            Body: JSON.stringify(metaData),
+            ContentType: "application/json",
+        })
+        .promise();
+    return `https://conflux-infty.s3.amazonaws.com/${sha}`;
 }
 
 // TODO
