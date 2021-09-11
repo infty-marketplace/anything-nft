ec2234fc10f5dee76a1674cc5dff2d9c0aad8977
Chungeng Meng <chungeng.meng@mail.utoronto.ca>
2021-09-10 22:20:13 -0400

    save

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", userRouter);
 app.use("/api", collectionRouter);
 
+app.use('/', express.static('src/views'))
+app.use('*', express.static('src/views'))
+
 db.on("error", (error) => console.error("MongoDB connection error: " + error.message));
 db.once("open", () => console.log("Connected to database"));
-
-app.use('/*', express.static('src/views'))
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
 
