e45501f72ac85a5dda80dcfe17aac97b57c63b52
William ji <williamji12345@gmail.com>
2021-11-01 15:36:35 -0400

    resolved #100

--- infty/src/router.js
+++ infty/src/router.js
@@ -1,59 +1,49 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 
-import CreatePage from "./pages/CreatePage.vue";
-import MainPage from "./pages/MainPage.vue";
-import Marketplace from "./pages/Marketplace";
-import Collections from "./pages/Collections.vue";
-import NftPage from "./pages/NftPage.vue";
-import AlbumPage from "./pages/AlbumPage";
-import RafflePage from "./pages/RafflePage";
-import ProfilePage from "./pages/ProfilePage";
-import StakePage from "./pages/StakePage";
+import CreatePage from './pages/CreatePage.vue';
+import MainPage from './pages/MainPage.vue';
+import Marketplace from './pages/Marketplace';
+import Collections from './pages/Collections.vue';
+import NftPage from './pages/NftPage.vue';
+import AlbumPage from './pages/AlbumPage';
+import ProfilePage from './pages/ProfilePage';
 
 Vue.use(Router);
 export default new Router({
-    mode: "history",
-    routes: [
-        {
-            path: "/",
-            component: MainPage,
-        },
-        {
-            path: "/marketplace",
-            component: Marketplace,
-        },
-        {
-            path: "/mine/create",
-            component: CreatePage,
-        },
-        {
-            path: "/mine/collections",
-            component: Collections,
-        },
-        {
-            path: "/raffles",
-            component: RafflePage,
-        },
-        {
-            path: "/nft/:id",
-            name: "nft-detail",
-            component: NftPage,
-            props: true,
-        },
-        {
-            path: "/album/:id",
-            name: "album-detail",
-            component: AlbumPage,
-            props: true,
-        },
-        {
-            path: "/profile/:address",
-            component: ProfilePage,
-        },
-        {
-            path: "/stake",
-            component: StakePage,
-        },
-    ],
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: MainPage,
+    },
+    {
+      path: '/marketplace',
+      component: Marketplace,
+    },
+    {
+      path: '/mine/create',
+      component: CreatePage,
+    },
+    {
+      path: '/mine/collections',
+      component: Collections,
+    },
+    {
+      path: '/nft/:id',
+      name: 'nft-detail',
+      component: NftPage,
+      props: true,
+    },
+    {
+      path: '/album/:id',
+      name: 'album-detail',
+      component: AlbumPage,
+      props: true,
+    },
+    {
+      path: '/profile/:address',
+      component: ProfilePage,
+    },
+  ],
 });
