74a9388df5f0dee83225b782b8ead9ebd8ec83d7
Lizhi Yu <leedsyu@yahoo.com>
2022-04-20 23:10:17 -0700

    add limiter

--- backend/src/routes/nftRouter.js
+++ backend/src/routes/nftRouter.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const nftController = require("../controllers/nftController");
 const multipart = require("connect-multiparty");
+const rateLimit = require("express-rate-limit");
 
 const multipartMiddleware = multipart();
+const createLimiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 24 hours
+    max: 100, // Limit each IP to 5 create account requests per `window`
+    message: "Too many accounts created from this IP, please try again after 24 hours",
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const router = express.Router();
 
 router.post("/market", nftController.getMarket);
 
 router.get("/nft/:nft_id", nftController.getNft);
-router.post("/create-nft", multipartMiddleware, nftController.createNft);
+router.post("/create-nft", multipartMiddleware, createLimiter, nftController.createNft);
 router.post("/delete-nft", nftController.deleteNft);
 router.post("/mint-estimate", nftController.getMintEstimate);
 router.post("/list-nft", nftController.listNft);
