170070e7c3da99a84cd9dc59387484ec04c7f6f9
Lizhi Yu <39842567+yuliwilliam@users.noreply.github.com>
2022-04-21 10:01:55 -0700

    Update nftRouter.js

--- backend/src/routes/nftRouter.js
+++ backend/src/routes/nftRouter.js
@@ -6,7 +6,7 @@ const rateLimit = require("express-rate-limit");
 const multipartMiddleware = multipart();
 const createLimiter = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 24 hours
-    max: 100, // Limit each IP to 5 create account requests per `window`
+    max: 100, // Limit each IP to 100 create account requests per `window`
     message: "Too many accounts created from this IP, please try again after 24 hours",
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
