28eae3cae3b1762d13b6d46ecc71c0c5675db996
YANLING_H <yanling.hu@mail.utoronto.ca>
2022-08-02 20:58:15 -0400

    finish

--- backend/src/controllers/nftController.js
+++ backend/src/controllers/nftController.js
@@ -108,6 +108,7 @@ async function createNft(req, res) {
         author: address,
         owner: [{ address: address, percentage: 1 }],
         labels: JSON.parse(req.body.labels),
+        unlockable_content: {text: req.body.unlockable_text, image: req.body.unlockable_image }
     };
     const newNft = new Nft(params);
     const user = await User.findOne({ address: address });
@@ -177,6 +178,7 @@ async function getMintEstimate(req, res) {
     try {
         const uri = "https://ipfs.io/ipfs/00000000000000000000000000000000000000000000000000000000000";
         const cost = await cfxUtils.mintEstimate(process.env.MANAGER_ADDRESS, uri);
+
         return res.json({ gas: cost.toString() });
     } catch (error) {
         return res.status(500).send(error);
--- backend/src/utils/cfxUtils.js
+++ backend/src/utils/cfxUtils.js
@@ -30,7 +30,11 @@ async function burn(tokenId) {
 // Multiply by 1.2 to avoid transaction failing due to potential underestimation
 async function mintEstimate(addr, uri) {
     const estimate = await minterContract.mint(addr, uri).estimateGasAndCollateral();
-    return (estimate.gasLimit + estimate.storageCollateralized) * 1.2;
+    const estimated_gas = estimate.gasLimit + estimate.storageCollateralized
+    return estimated_gas / 5n + estimated_gas;
+    // both estimate.gasLimit and esitmate.storageCollateralized are bigInt which can't be multiplied by a float directly
+    // also, since bigInt is an integer, we use integer division instead of division in the formula below
+    // value * 1.2 is approximated by  value / 5 + value
 }
 
 async function createRaffle(details) {
