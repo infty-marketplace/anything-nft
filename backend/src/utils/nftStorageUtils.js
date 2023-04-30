396c87e49d3a3a228c2a977c95802c22dcb309b2
Haohua Ji <williamhaohua.ji@mail.utoronto.ca>
2023-04-30 06:14:29 -0400

    moved timeout function inside so that it waits between each try

--- backend/src/utils/nftStorageUtils.js
+++ backend/src/utils/nftStorageUtils.js
@@ -5,42 +5,43 @@ const axios = require("axios");
 const { NFTStorage, File } = require("nft.storage");
 
 function ipfsToHttps(url) {
-    return url.replace("ipfs://", "https://ipfs.io/ipfs/");
+  return url.replace("ipfs://", "https://ipfs.io/ipfs/");
 }
 
 function urlToCid(url) {
-    const regex = new RegExp("https://ipfs.io/ipfs/([a-z0-9]+)/", "g");
-    const matches = regex.exec(url);
-    return matches[1];
+  const regex = new RegExp("https://ipfs.io/ipfs/([a-z0-9]+)/", "g");
+  const matches = regex.exec(url);
+  return matches[1];
 }
 
 async function upload(filePath, name, description) {
-    const content = await fs.promises.readFile(filePath);
-    const type = mime.getType(filePath);
-    const image = new File([content], path.basename(filePath), { type });
+  const content = await fs.promises.readFile(filePath);
+  const type = mime.getType(filePath);
+  const image = new File([content], path.basename(filePath), { type });
 
-    const nftstorage = new NFTStorage({ token: process.env.NFT_STORAGE_KEY });
-    const receipt = await nftstorage.store({ image, name, description });
-    const metadataUrl = ipfsToHttps(receipt.url);
-    return metadataUrl;
+  const nftstorage = new NFTStorage({ token: process.env.NFT_STORAGE_KEY });
+  const receipt = await nftstorage.store({ image, name, description });
+  const metadataUrl = ipfsToHttps(receipt.url);
+  return metadataUrl;
 }
 
 async function burn(url) {
-    const nftstorage = new NFTStorage({ token: process.env.NFT_STORAGE_KEY });
-    await nftstorage.delete(urlToCid(url)).catch((e) => {
-        // if it does not exist, we can ignore the error and move on
-        if (e.message === "NFT not found") return;
-        throw e;
-    });
+  const nftstorage = new NFTStorage({ token: process.env.NFT_STORAGE_KEY });
+  await nftstorage.delete(urlToCid(url)).catch((e) => {
+    // if it does not exist, we can ignore the error and move on
+    if (e.message === "NFT not found") return;
+    throw e;
+  });
 }
 
 async function getImageUrl(metadataUrl) {
-    for (let i = 0; i < 10; i++) {
-        const res = await axios.get(metadataUrl);
-        if (res && res.data.image) {
-            return ipfsToHttps(res.data.image);
-        }
+  for (let i = 0; i < 10; i++) {
+    const res = await axios.get(metadataUrl);
+    if (res && res.data.image) {
+      return ipfsToHttps(res.data.image);
     }
     await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
 }
+
 module.exports = { upload, getImageUrl, burn };
